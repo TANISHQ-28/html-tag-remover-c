610fc7dc3441f169215f728309b6fb2ffe9cb58d
TANISHQ-28 <tanishqsoni2807@gmail.com>
2025-07-24 14:04:06 +0530

    Add files via upload

---
+++ htmlparser.c
@@ -0,0 +1,50 @@
+#include<stdio.h>
+#include<string.h>
+
+void parser(char *string)
+{
+    int in=0, index =0;
+    for(int i=0;i<strlen(string);i++)
+    {
+        if(string[i]=='<')
+        {
+            in=1;
+            continue;
+        }
+        else if(string[i]=='>')
+        {  
+            in =0;
+            continue;
+        }
+        if(in==0)
+        {
+            string[index]=string[i];
+            index++;
+            continue;
+        }
+    }
+    string[index]='\0';
+    //removing trailing spaces from the beginning
+
+    while(string[0]==' ')
+    {
+        for(int i=0;i<strlen(string);i++)
+        {
+            string[i]=string[i+1];
+
+        }
+    }
+    //removing trailing spaces from the end
+
+    while(string[strlen(string)-1]==' ') //[strlen(string) - 1] isiliye kiya hai kyuki aakhri character hamara null character hoga 
+    {
+        string[strlen(string)-1] = '\0';
+    }
+}
+int main()
+{
+   char str[]= "<hi>     HI MY name is TANISHQ      <hi>";
+   parser(str);
+   printf("Parsed string ~~ %s ~~",str);
+   return 0;
+}
